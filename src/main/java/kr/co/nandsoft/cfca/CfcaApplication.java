a984d277c7f15253bf45b07b2aed2635b060f912
cleanbrain <46418030+cleanbrain@users.noreply.github.com>
2022-04-08 17:04:08 +0900

    first commit

---
+++ src/main/java/kr/co/nandsoft/cfca/CfcaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.nandsoft.cfca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfcaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfcaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/nandsoft/cfca/CfcaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.nandsoft.cfca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfcaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
